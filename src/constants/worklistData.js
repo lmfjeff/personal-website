7cfd648c0a8303378f4743f2571555a918e40bc6
lmfjeff <lmfjeff@gmail.com>
2022-03-16 16:57:05 +0800

    modularize worklist component, separate project data

---
+++ src/constants/worklistData.js
@@ -0,0 +1,28 @@
+const worklistData = [
+  {
+    projectName: 'Anime Notifier',
+    description: 'A seasonal anime website built with Next.js',
+    url: 'https://anime.lmfjeff.com/',
+    githubUrl: 'https://github.com/lmfjeff/anime-notifier',
+    techStack: ['Next.js', 'React', 'Dynamodb'],
+    image: 'anime-notifier.jpg',
+  },
+  {
+    projectName: 'Task Tracker',
+    description: 'To-do list synced with online database',
+    url: 'https://task-tracker-210517.herokuapp.com/',
+    githubUrl: 'https://github.com/lmfjeff/Task-Tracker',
+    techStack: ['React', 'Express js', 'MongoDB', 'JWT auth', 'bcrypt encrytion'],
+    image: 'task-tracker.png',
+  },
+  {
+    projectName: 'Firebase Chat',
+    description: 'Online instant chat app using firebase backend',
+    url: 'https://fir-chat-56261.web.app/',
+    githubUrl: 'https://github.com/lmfjeff/firebase-chat',
+    techStack: ['React', 'Firebase firestore', 'Firebase auth'],
+    image: 'firebase-chat.png',
+  },
+]
+
+export default worklistData
